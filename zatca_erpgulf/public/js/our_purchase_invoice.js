bcfb5e1074b5ee0204497cefec310062241fb2ce
kanchan <kittumahajan581@gmail.com>
2025-03-26 16:19:14 +0530

    fix update code for hide field in sales purchase invoice

--- zatca_erpgulf/public/js/our_purchase_invoice.js
+++ zatca_erpgulf/public/js/our_purchase_invoice.js
@@ -1,29 +1,9 @@
-frappe.ui.form.on('Purchase Invoice',{
-
+frappe.ui.form.on('Purchase Invoice', {
     refresh: function(frm) {        
-        if (frm.doc.company) {
-            frappe.db.get_value("Company", frm.doc.company, "country", (r) => {
-                if (r.country != "Saudi Arabia") {
-                    console.log("working------------------------->")
-                    frm.set_df_property('custom_zatca_third_party_invoice', 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_nominal_invoice', 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_export_invoice','hidden',1);
-                    frm.set_df_property('custom_summary_invoice','hidden',1);
-                    frm.set_df_property('custom_self_billed_invoice','hidden',1);
-                    frm.set_df_property('custom_zatca_status' ,'hidden' , 1);
-                    frm.set_df_property('custom_zatca_tax_category' , 'hidden' , 1);
-                    frm.set_df_property('custom_exemption_reason_code' , 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_discount_reason_code' , 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_discount_reason' , 'hidden' , 1);
-                    frm.set_df_property('custom_submit_line_item_discount_to_zatca' , 'hidden' , 1);
-                    $('a[data-fieldname="custom_offlineintegrations"]').hide();
-                    
-                    
-                }
-                
-            });
-        }
-
+        // Check company country and hide/show fields
+        update_field_visibility(frm);
+        
+        // Add ZATCA button if conditions are met
         if (frm.doc.docstatus === 1 && !["CLEARED", "REPORTED"].includes(frm.doc.custom_zatca_status)) {
             frappe.db.get_value("Company", frm.doc.company, "country", (r) => {
                 if (r.country === "Saudi Arabia") {
@@ -44,48 +24,68 @@ frappe.ui.form.on('Purchase Invoice',{
         }
     },
     
-    company: function(frm){
-        if (frm.doc.company) {
-            frappe.db.get_value("Company", frm.doc.company, "country", (r) => {
-
-                if (r.country != "Saudi Arabia") {
-
-                    frm.set_df_property('custom_zatca_third_party_invoice', 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_nominal_invoice', 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_export_invoice','hidden',1);
-                    frm.set_df_property('custom_summary_invoice','hidden',1);
-                    frm.set_df_property('custom_self_billed_invoice','hidden',1);
-                    frm.set_df_property('custom_zatca_status' ,'hidden' , 1);
-                    frm.set_df_property('custom_zatca_tax_category' , 'hidden' , 1);
-                    frm.set_df_property('custom_exemption_reason_code' , 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_discount_reason_code' , 'hidden' , 1);
-                    frm.set_df_property('custom_zatca_discount_reason' , 'hidden' , 1);
-                    frm.set_df_property('custom_submit_line_item_discount_to_zatca' , 'hidden' , 1);
-                    $('a[data-fieldname="custom_offlineintegrations"]').hide();
-                    
-                    
-                }
-                else {
-                    frm.set_df_property('custom_zatca_third_party_invoice', 'hidden' , 0);
-                    frm.set_df_property('custom_zatca_nominal_invoice', 'hidden' , 0);
-                    frm.set_df_property('custom_zatca_export_invoice','hidden',0);
-                    frm.set_df_property('custom_summary_invoice','hidden',0);
-                    frm.set_df_property('custom_self_billed_invoice','hidden',0);
-                    frm.set_df_property('custom_zatca_status' ,'hidden' , 0);
-                    frm.set_df_property('custom_zatca_tax_category' , 'hidden' , 0);
-                    frm.set_df_property('custom_exemption_reason_code' , 'hidden' , 0);
-                    frm.set_df_property('custom_zatca_discount_reason_code' , 'hidden' , 0);
-                    frm.set_df_property('custom_zatca_discount_reason' , 'hidden' , 0);
-                    frm.set_df_property('custom_submit_line_item_discount_to_zatca' , 'hidden' , 0);
-                    frm.set_df_property('custom_offlineintegrations', True);
-                    
-                }
-            });
-        }
+    company: function(frm) {
+        update_field_visibility(frm);
     }   
+});
 
-})
+// Common function to update field visibility based on company country
+function update_field_visibility(frm) {
+    if (!frm.doc.company) {
+        // If no company selected, hide all fields
+        hide_zatca_fields(frm);
+        return;
+    }
+    
+    frappe.db.get_value("Company", frm.doc.company, "country", (r) => {
+        if (r.country === "Saudi Arabia") {
+            show_zatca_fields(frm);
+        } else {
+            hide_zatca_fields(frm);
+        }
+    });
+}
+function hide_zatca_fields(frm) {
+    const fields_to_hide = [
+        'custom_zatca_third_party_invoice',
+        'custom_zatca_nominal_invoice',
+        'custom_zatca_export_invoice',
+        'custom_summary_invoice',
+        'custom_self_billed_invoice',
+        'custom_zatca_status',
+        'custom_zatca_tax_category',
+        'custom_exemption_reason_code',
+        'custom_zatca_discount_reason_code',
+        'custom_zatca_discount_reason',
+        'custom_submit_line_item_discount_to_zatca'
+    ];
+    
+    fields_to_hide.forEach(field => {
+        frm.set_df_property(field, 'hidden', 1);
+    });
+    
+    $('a[data-fieldname="custom_offlineintegrations"]').hide();
+}
 
-
-
-
+function show_zatca_fields(frm) {
+    const fields_to_show = [
+        'custom_zatca_third_party_invoice',
+        'custom_zatca_nominal_invoice',
+        'custom_zatca_export_invoice',
+        'custom_summary_invoice',
+        'custom_self_billed_invoice',
+        'custom_zatca_status',
+        'custom_zatca_tax_category',
+        'custom_exemption_reason_code',
+        'custom_zatca_discount_reason_code',
+        'custom_zatca_discount_reason',
+        'custom_submit_line_item_discount_to_zatca',
+        'custom_offlineintegrations'
+    ];
+    
+    fields_to_show.forEach(field => {
+        frm.set_df_property(field, 'hidden', 0);
+    });
+    
+    $('a[data-fieldname="custom_offlineintegrations"]').show();
+}
