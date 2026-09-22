7717947d557aed9abe58bf0e9704db5f3fc52c05
Apoorv Soral <soral.apoorv@gmail.com>
2026-09-22 10:54:53 +0530

    test: cover ZATCA event log fields

--- zatca_erpgulf/zatca_erpgulf/doctype/zatca_erpgulf_event_log/test_zatca_erpgulf_event_log.py
+++ zatca_erpgulf/zatca_erpgulf/doctype/zatca_erpgulf_event_log/test_zatca_erpgulf_event_log.py
@@ -1,9 +1,22 @@
 # Copyright (c) 2025, ERPGulf and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestZATCAERPGulfEventLog(FrappeTestCase):
-	pass
+    def test_event_log_fields(self):
+        doc = frappe.get_doc({
+            "doctype": "ZATCA ERPGulf Event Log",
+            "title": "Test ZATCA Event",
+            "invoice_number": "TEST-INV",
+            "time": "2026-09-22 10:00:00",
+            "api_response": "test response",
+            "status": "success",
+            "custom_uuid": "test-uuid",
+        })
+        doc.insert(ignore_permissions=True)
+        self.assertEqual(doc.status, "success")
+        self.assertEqual(doc.custom_uuid, "test-uuid")
+        doc.delete(ignore_permissions=True)
