e1ed873c8be284901017dfd0bd92073f58d36085
Apoorv Soral <soral.apoorv@gmail.com>
2026-09-22 13:14:19 +0530

    fix: preserve duplicate ZATCA responses in pos_submit_with_xml_qr.py

--- zatca_erpgulf/zatca_erpgulf/pos_submit_with_xml_qr.py
+++ zatca_erpgulf/zatca_erpgulf/pos_submit_with_xml_qr.py
@@ -207,7 +207,7 @@ def reporting_api_machine(
                     )
                 )
 
-            if response.status_code not in (200, 202):
+            if response.status_code not in (200, 202, 409):
                 invoice_doc = frappe.get_doc("POS Invoice", invoice_number)
                 invoice_doc.db_set(
                     "custom_uuid", "Not Submitted", commit=True, update_modified=True
