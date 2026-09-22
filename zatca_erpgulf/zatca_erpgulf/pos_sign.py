905f9c1ef7182aafe41630e95c7ff5c1273f3022
Apoorv Soral <soral.apoorv@gmail.com>
2026-09-22 10:39:02 +0530

    fix: centralize reporting CSID resolution

--- zatca_erpgulf/zatca_erpgulf/pos_sign.py
+++ zatca_erpgulf/zatca_erpgulf/pos_sign.py
@@ -12,6 +12,7 @@ import frappe
 from zatca_erpgulf.zatca_erpgulf.event_log import log_zatca_event
 from zatca_erpgulf.zatca_erpgulf.zatca_context import (
     get_zatca_company_context,
+    get_zatca_credential_context,
     validate_zatca_invoice_before_submission,
 )
 from zatca_erpgulf.zatca_erpgulf.posxml import (
@@ -357,13 +358,10 @@ def clearance_api(
         if not company_doc.is_group and company_doc.parent_company and company_doc.custom_costcenter:
             company_doc = frappe.get_doc("Company",company_doc.parent_company)
             company_abbr = company_doc.abbr
-        if pos_invoice_doc.custom_zatca_pos_name:
-            zatca_settings = frappe.get_doc(
-                "Zatca Multiple Setting", pos_invoice_doc.custom_zatca_pos_name
-            )
-            production_csid = zatca_settings.custom_final_auth_csid
-        else:
-            production_csid = company_doc.custom_basic_auth_from_production or ""
+        credential_context = get_zatca_credential_context(
+            get_zatca_company_context(pos_invoice_doc)
+        )
+        production_csid = credential_context.get("csid") or ""
         payload = {
             "invoiceHash": encoded_hash,
             "uuid": uuid1,
